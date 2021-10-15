75af3c3f0dbc923d7b0d9d251a0d8ff5d550056e
Julien <jbuisine59@gmail.com>
2021-10-15 13:34:33 +0200

    fix bug decompo

--- tp2/decomp_util.py
+++ tp2/decomp_util.py
@@ -10,13 +10,13 @@ def decomposition(array) :
 
     for index in range(0, size):
         # x_i^n value
-        value = 1/4 * ( (-array[((2*index)-2)%size] + 3*array[(2*index-1)%size] + 3*array[(2*index)%size] - array[((2*index)+1)%size] )
+        value = 1/4 * ( (-array[((2*index)-2)%size] + 3*array[(2*index-1)%size] + 3*array[(2*index)%size] - array[((2*index)+1)%size] ))
         x_array = np.append(x_array, value)
 
         # y_i^n value
-        value = 1/4 * ( array[((2*index)-2)%size] - 3*array[(2*index-1)%size] + 3*array[(2*index)%size] - array[((2*index)+1)%size] )
-        y_ array = np.append(y_array, value)
-    
+        value = 1/4 * ( (array[((2*index)-2)%size] - 3*array[(2*index-1)%size] + 3*array[(2*index)%size] - array[((2*index)+1)%size] ))
+        y_array = np.append(y_array, value)
+
     res_partiel = np.concatenate([x_array, y_array])
 
     return res
