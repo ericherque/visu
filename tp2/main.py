7a5dd1212208d96d4d7af1758f2dba8e46b34d51
Eric Herque <eric.herque@etu.univ-grenoble-alpes.fr>
2021-10-15 15:36:37 +0200

    eric.new

--- tp2/main.py
+++ tp2/main.py
@@ -5,23 +5,25 @@ import math
 
 #np.loadtxt() 
 #np.savetxt()
+#np.mod(array, size)
 
 ## decomposition totale de chaikin
 def decomposition(array) :
     x_array = np.array([])
     y_array = np.array([])
-    size = len(array) - 1
+    size = len(array)
 
     for index in range(0, size):
         # x_i^n value
-        value = 1/4 * (-array[(2*index)-2] + 3*array[2*index-1] + 3*array[(2*index)] - array[(2*index)+1])
+        value = 1/4 * ( (-array[((2*index)-2)%size] + 3*array[(2*index-1)%size] + 3*array[(2*index)%size] - array[((2*index)+1)%size] ))
         x_array = np.append(x_array, value)
 
         # y_i^n value
-        value = 1/4 * (array[(2*index)-2] - 3*array[2*index-1] + 3*array[2*index] - array[(2*index)+1])
+        value = 1/4 * ( (array[((2*index)-2)%size] - 3*array[(2*index-1)%size] + 3*array[(2*index)%size] - array[((2*index)+1)%size] ))
         y_array = np.append(y_array, value)
 
     res = np.concatenate([x_array, y_array])
+    print(res)
 
 ## recomposition totale de chaikin
 def recomposition(array) :
@@ -33,9 +35,14 @@ def recomposition(array) :
 
 
     #res = 
-    
+
 def main() :
     herisson = np.loadtxt("herisson512.d")
-    decomp_herisson = decomposition(herisson)
+
+    np_herisson = np.array(herisson)
+    decomp_herisson = decomposition(np_herisson)
+
     plt.plot(herisson)
     plt.show()
+
+main()
