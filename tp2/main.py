2c39a5568a8e1c9cebd171315826baab5385b72b
Eric Herque <eric.herque@etu.univ-grenoble-alpes.fr>
2021-10-15 14:25:03 +0200

    fuck les modules

--- tp2/main.py
+++ tp2/main.py
@@ -2,14 +2,40 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 
-import decomp_util as dcp
-import recomp_util as rcp
 
 #np.loadtxt() 
 #np.savetxt()
 
+## decomposition totale de chaikin
+def decomposition(array) :
+    x_array = np.array([])
+    y_array = np.array([])
+    size = len(array) - 1
+
+    for index in range(0, size):
+        # x_i^n value
+        value = 1/4 * (-array[(2*index)-2] + 3*array[2*index-1] + 3*array[(2*index)] - array[(2*index)+1])
+        x_array = np.append(x_array, value)
+
+        # y_i^n value
+        value = 1/4 * (array[(2*index)-2] - 3*array[2*index-1] + 3*array[2*index] - array[(2*index)+1])
+        y_array = np.append(y_array, value)
+
+    res = np.concatenate([x_array, y_array])
+
+## recomposition totale de chaikin
+def recomposition(array) :
+    x_array = np.array([])
+    y_array = np.array([])
+    size = len(array) - 1
+
+    #for index in range(0, size):
+
+
+    #res = 
+    
 def main() :
     herisson = np.loadtxt("herisson512.d")
-    decomp_herisson = dcp.decompositon(herisson)
+    decomp_herisson = decomposition(herisson)
     plt.plot(herisson)
     plt.show()
