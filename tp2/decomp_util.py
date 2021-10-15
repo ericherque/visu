0b07da9efa973d45e627e8af583ef41414e32fe2
Eric Herque <eric.herque@etu.univ-grenoble-alpes.fr>
2021-10-15 13:58:31 +0200

    decomp impl. (à tester)

--- tp2/decomp_util.py
+++ tp2/decomp_util.py
@@ -10,14 +10,11 @@ def decomposition(array) :
 
     for index in range(0, size):
         # x_i^n value
-        value = 1/4 * ( (-array[((2*index)-2)%size] + 3*array[(2*index-1)%size] + 3*array[(2*index)%size] - array[((2*index)+1)%size] ))
+        value = 1/4 * (-array[(2*index)-2] + 3*array[2*index-1] + 3*array[(2*index)] - array[(2*index)+1])
         x_array = np.append(x_array, value)
 
         # y_i^n value
-        value = 1/4 * ( (array[((2*index)-2)%size] - 3*array[(2*index-1)%size] + 3*array[(2*index)%size] - array[((2*index)+1)%size] ))
+        value = 1/4 * (array[(2*index)-2] - 3*array[2*index-1] + 3*array[2*index] - array[(2*index)+1])
         y_array = np.append(y_array, value)
 
-    res_partiel = np.concatenate([x_array, y_array])
-
-    return res_partiel
-
+    res = np.concatenate([x_array, y_array])
--- tp2/recomp_util.py
+++ tp2/recomp_util.py
@@ -3,4 +3,11 @@ import math
 
 ## recomposition totale de chaikin
 def recomposition(array) :
-    
+    x_array = np.array([])
+    y_array = np.array([])
+    size = len(array) - 1
+
+    #for index in range(0, size):
+
+
+    #res = 
