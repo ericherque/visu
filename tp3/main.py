d936c992120777040a7283263a5784088d4380f0
Eric Herque <eric.herque@etu.univ-grenoble-alpes.fr>
2021-10-19 15:45:13 +0200

    init .py file

---
+++ tp3/main.py
@@ -0,0 +1,12 @@
+#Entree: 
+#   - une liste de points {X_1, ..., X_n} dans R^2
+#   - une liste de valeurs {f_1, ..., f_n}
+#   - les paramètres {µ_1, ..., µ_n}
+#   - un point X
+#
+#Sortie:
+#   > F(X)
+#
+def funcSheperd(points, values, params, x):
+    #TODO
+
