34edf3ff75fa8905906e195072691ff46c1b3e6a
Eric Herque <eric.herque@etu.univ-grenoble-alpes.fr>
2021-11-19 14:47:57 +0100

    maj fonctions

--- tp4/main.py
+++ tp4/main.py
@@ -11,12 +11,54 @@ def distance2(x1, x2, y1, y2):
 	return math.sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2))
 
 
-def pointweight(X, Y, i, x, y, mu):
+def numerator_calcul(x, y, X, Y, i, mu):
+	## (x,y) = le point qu'on a choisi
+	## X = tableau des x
+	## Y = tableau des y
+	## i = l'indice i de Wi
+	## mu = pour l'instant on met une valeur fixe
+	res = 1
+	index = 0
+	for (tabX, tabY) in zip(X, Y):
+		for (eltX, eltY) in zip(tabX, tabY):
+			if index != i:
+				res *= pow(distance2(x, eltX, y, eltY), mu)
+	return res
+
+def denominator_calcul(x, y, X, Y, i, mu):
+	res = 0
+	mult_inter = 1
+	X_array = []
+	Y_array = []
+	index = 0
+	## init
+	for (tabX, tabY) in (X, Y):
+		for (eltX, eltY) in (tabX, tabY):
+			X_array[index] = eltX
+			Y_array[index] = eltY
+			index += 1
+
+	for j in range(0, len(X_array)-1):
+		for k in range(0, len(X_array)-1):
+				if j!=k:
+					mult_inter *= pow(distance2(x, X_array[k], y, Y_array[k]), mu)
+			
+		res += mult_inter
+	return res
+
+
+def pointweight(x, y, X, Y, i, mu):
+	numerator = numerator_calcul(x, y, X, Y, i, mu)
+	denominator = denominator_calcul(x, y, X, Y, i, mu)
+	return numerator/denominator
+
+
+def pointweight_old(X, Y, i, x, y, mu):
 
 	numerator = 1
 	denominator = 0
 
-	for k in range(0, len(x)-1):
+	for k in range(0, len(x[0])-1):
 		# résultat de la multiplication (pour dénominateur)
 		mult_inter = 1
 
@@ -38,9 +80,14 @@ def shepard(X, Y, x, y, values, mu):
 	#somme de {wi(X) * fi}
 	F = 0
 	w = 0
+	Z_array = []
+	for tab in values:
+		for eltZ in tab:
+			Z_array.append(eltZ)
+
 	for i in range(0, len(x)-1):
 		w = pointweight(X, Y, i, x, y, mu)
-		w *= values[i]
+		w *= Z_array[i]
 		F += w
 ######
 # Dessin surface
@@ -55,7 +102,8 @@ Zf = np.cos(X/2.) * np.sin(X**2 + Y)
 #
 x_random = 2
 y_random = 7
-shepard(x_random, y_random, X, Y, Zf, mu)
+shepard(x_random, y_random, X, Y, Zf, 2)
+
 #
 #
 fig = plt.figure()
@@ -71,5 +119,6 @@ ax.scatter( [0.2, 0.8, 0.1],[0.3, 0.5, 0.7],[0.5, 0.5, 0.5], marker="^")
 ax.set_xlabel('X axis')
 ax.set_ylabel('Y axis')
 ax.set_zlabel('Z axis')
+
 
 plt.show()
