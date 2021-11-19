295cf6fc3361d1338776b0019885e1d7e6ba8795
Eric Herque <eric.herque@etu.univ-grenoble-alpes.fr>
2021-11-19 15:13:53 +0100

    impl. et compil. OK

--- tp4/main.py
+++ tp4/main.py
@@ -11,12 +11,12 @@ def distance2(x1, x2, y1, y2):
 	return math.sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2))
 
 
-def numerator_calcul(x, y, X, Y, i, mu):
-	## (x,y) = le point qu'on a choisi
+## (x,y) = le point qu'on a choisi
 	## X = tableau des x
 	## Y = tableau des y
 	## i = l'indice i de Wi
 	## mu = pour l'instant on met une valeur fixe
+def numerator_calcul(x, y, X, Y, i, mu):
 	res = 1
 	index = 0
 	for (tabX, tabY) in zip(X, Y):
@@ -25,19 +25,23 @@ def numerator_calcul(x, y, X, Y, i, mu):
 				res *= pow(distance2(x, eltX, y, eltY), mu)
 	return res
 
+
+## (x,y) = le point qu'on a choisi
+## X = tableau des x
+## Y = tableau des y
+## i = l'indice i de Wi
+## mu = pour l'instant on met une valeur fixe
 def denominator_calcul(x, y, X, Y, i, mu):
 	res = 0
 	mult_inter = 1
 	X_array = []
 	Y_array = []
-	index = 0
-	## init
-	for (tabX, tabY) in (X, Y):
-		for (eltX, eltY) in (tabX, tabY):
-			X_array[index] = eltX
-			Y_array[index] = eltY
-			index += 1
-
+	#init des tab de X et Y
+	for (tabX, tabY) in zip(X, Y):
+		for (eltX, eltY) in zip(tabX, tabY):
+			X_array.append(eltX)
+			Y_array.append(eltY)
+	#calcul du dénominateur
 	for j in range(0, len(X_array)-1):
 		for k in range(0, len(X_array)-1):
 				if j!=k:
@@ -48,35 +52,14 @@ def denominator_calcul(x, y, X, Y, i, mu):
 
 
 def pointweight(x, y, X, Y, i, mu):
+	##calcul du Wi
 	numerator = numerator_calcul(x, y, X, Y, i, mu)
+	print(numerator)
 	denominator = denominator_calcul(x, y, X, Y, i, mu)
 	return numerator/denominator
 
 
-def pointweight_old(X, Y, i, x, y, mu):
-
-	numerator = 1
-	denominator = 0
-
-	for k in range(0, len(x[0])-1):
-		# résultat de la multiplication (pour dénominateur)
-		mult_inter = 1
-
-		# calcul du numérateur sans k=i
-		if k!=i:
-			numerator *= pow(distance2(X, x[k], Y, y[k]), mu[k])
-
-		#calcul du dénominateur sans k=j
-		for j in range(0, len(x)-1):
-			if j!=k:
-				mult_inter *= pow(distance2(X, x[j], Y, y[j]), mu[j])
-		
-		denominator += mult_inter
-				
-	return numerator/denominator
-
-
-def shepard(X, Y, x, y, values, mu):
+def shepard(x, y, X, Y, values, mu):
 	#somme de {wi(X) * fi}
 	F = 0
 	w = 0
@@ -85,10 +68,11 @@ def shepard(X, Y, x, y, values, mu):
 		for eltZ in tab:
 			Z_array.append(eltZ)
 
-	for i in range(0, len(x)-1):
-		w = pointweight(X, Y, i, x, y, mu)
+	for i in range(0, len(Z_array)-1):
+		w = pointweight(x, y, X, Y, i, mu)
 		w *= Z_array[i]
 		F += w
+	return F
 ######
 # Dessin surface
 xi = np.linspace( 0., 1., num=10)
@@ -102,8 +86,8 @@ Zf = np.cos(X/2.) * np.sin(X**2 + Y)
 #
 x_random = 2
 y_random = 7
-shepard(x_random, y_random, X, Y, Zf, 2)
-
+F = shepard(x_random, y_random, X, Y, Zf, 2)
+print(F)
 #
 #
 fig = plt.figure()
